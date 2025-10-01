9741f611c4403f1eeb293425484f16f85cafea94
TheCookieLegend <lucas2.olafsen@gmail.com>
2025-10-01 16:50:21 +0200

    Dice spawning works now and can be called from other classes/objects

---
+++ Dice Roller/Assets/Scripts/DiceSpawner.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DiceSpawner : MonoBehaviour
+{
+    //Dice should spawn between (-2.5, 17, 9.5) and (5.2, 17, -6.6)
+    [SerializeField] private GameObject[] diceObject;
+    public static DiceSpawner Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject); // optional safeguard
+        }
+    }
+
+
+    public void SpawnDice(int numberOfDice) {
+        for (int i = 0; i < numberOfDice; i++) {
+            // figure out spawn location
+            Vector3 spawnLocation = new Vector3(Random.Range(-2.5f, 5.2f), 17, Random.Range(-6.6f, 9.5f));
+            // figure out spawn rotation
+            Quaternion spawnRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            // spawn in the dice
+            Instantiate(diceObject[Random.Range(0, numberOfDice)], spawnLocation, spawnRotation);
+        }
+    }
+
+}
