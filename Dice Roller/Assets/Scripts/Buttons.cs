3d7a1c6f53275f95ce615039dcc0091d3571535f
TheCookieLegend <lucas2.olafsen@gmail.com>
2025-10-03 14:31:49 +0200

    You can now choose how many dice will be rolled
    A maximum and minimum has also been set: Min 1 and max 8

--- Dice Roller/Assets/Scripts/Buttons.cs
+++ Dice Roller/Assets/Scripts/Buttons.cs
@@ -4,20 +4,45 @@ using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
-    private int numberOfDice = 1;
+    private int numberOfDice = 2;
     [SerializeField] private TMP_Text diceText;
     [SerializeField] private Button plusButton, minusButton;
-    
+
+    void Start() {
+        diceText.text = numberOfDice + " dice";
+    }
+
     public void RollDice() {
         DiceSpawner.Instance.SpawnDice(numberOfDice);
     }
 
     public void IncreaseDice() {
         numberOfDice++;
-
+        ButtonInteractable();
+        UpdateText();
     }
 
     public void DecreaseDice() {
         numberOfDice--;
+        ButtonInteractable();
+        UpdateText();
+    }
+
+    public void ButtonInteractable() {
+        if (numberOfDice <= 1) {
+            minusButton.interactable = false;
+        } else {
+            minusButton.interactable = true;
+        }
+
+        if (numberOfDice >= 8) {
+            plusButton.interactable = false;
+        } else {
+            plusButton.interactable = true;
+        }
+    }
+
+    public void UpdateText() {
+        diceText.text = numberOfDice + " dice";
     }
 }
